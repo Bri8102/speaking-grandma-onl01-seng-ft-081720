03821d36131f45ad109b6d537cb608f21ab57105
Briana Oniru <damioniru16@gmail.com>
2020-08-18 19:11:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,8 @@
 def speak_to_grandma(phrase)
    if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-      puts "I LOVE YOU TOO PUMPKIN!"
-   else if phrase == phrase.upcase
-      return "NOT SINCE 1938!"
+   elseif phrase == phrase.upcase
+      return "NO, NOT SINCE 1938!"
    end
  else
      return "HUH! SPEAK UP, SONNY!"
